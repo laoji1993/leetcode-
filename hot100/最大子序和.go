0dd80c03658d602d45ea106dc4a5e6b43a0c9914
laoji1993 <2284221849@qq.com>
2021-03-01 23:11:06 +0800

    day2

---
+++ hot100/最大子序和.go
@@ -0,0 +1,18 @@
+package main
+
+func maxSubArray(nums []int) int {
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	max := dp[0]
+	for i := 1; i < len(nums); i++ {
+		if dp[i-1] <= 0 {
+			dp[i] = nums[i]
+		} else {
+			dp[i] = nums[i] + dp[i-1]
+		}
+		if dp[i] > max {
+			max = dp[i]
+		}
+	}
+	return max
+}
