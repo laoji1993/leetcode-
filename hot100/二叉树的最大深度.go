bfe51889d800932ccad1877ccaf32887c2f7c601
guanyu.ji <guanyu.ji@17zuoye.com>
2021-03-02 18:23:58 +0800

    day3

---
+++ hot100/二叉树的最大深度.go
@@ -0,0 +1,18 @@
+package main
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	l := maxDepth(root.Left)
+	r := maxDepth(root.Right)
+
+	return max(l, r) + 1
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
